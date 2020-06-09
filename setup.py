7f04a587ef4982fcb1cf8912061c6bcfde2fe4d2
Jonathan Aharon <jonathan.aharon@perception-point.io>
2020-06-09 12:20:09 +0300

    Add backup pylnker lib for parsing LNK files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pylnker',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/perceptionpoint/pylnker',
+    license='GNU General Public License v2.0',
+    author='HarmJ0y',
+    author_email='',
+    description='This is a Python port of lnk-parse-1.0, a tool to parse Windows .lnk files.  '
+)
